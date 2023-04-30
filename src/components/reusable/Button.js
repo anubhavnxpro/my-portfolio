ad92def38ae65abffaaefed2a7259824a6ecc064
Anubhav530 <anubhav3516.be20@chitkara.edu.in>
2023-04-30 22:43:16 +0530

    Image issue fixed

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -1,12 +1,13 @@
 import { ToastContainer } from 'react-toastify';
 import loadersvg from '../../../public/images/loader.svg'
+import Image from 'next/image';
 
 const Button = ({ title, loading = false, disabled }) => {
 	return (
 		<>
 			<button type='submit' className='submit-btn' disabled={disabled}>
 				{
-					!loading ? title : <img src={loadersvg.src} className='spinner' alt='Loading' />
+					!loading ? title : <Image width={13} height={14} layout='responsive' src={loadersvg.src} className='spinner' alt='Loading' />
 				}
 			</button>
 			<ToastContainer />

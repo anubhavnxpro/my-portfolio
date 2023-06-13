a4fef6ddc599fbc8b18e3be9437e00b9df26c808
anubhav singla <anubhav.singla@coriolis.co.in>
2023-06-13 14:19:25 +0530

    Added one project

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -187,4 +187,95 @@ export const projectsData = [
 			],
 		},
 	},
+
+	{
+		id: 3,
+		title: 'Cetas AI',
+		url: 'https://www.cetas.ai/',
+		category: 'Web Application',
+		img: '/images/mobile-project-2.jpg',
+		ProjectHeader: {
+			title: 'Cetas AI',
+			publishDate: 'May 05, 2020',
+			tags: 'Web Application',
+		},
+		ProjectImages: [
+			{
+				id: uuidv4(),
+				title: 'Cetas AI',
+				img: '/images/ui-project-1.jpg',
+			},
+			{
+				id: uuidv4(),
+				title: 'Cetas AI',
+				img: '/images/web-project-2.jpg',
+			},
+			{
+				id: uuidv4(),
+				title: 'Cetas AI',
+				img: '/images/mobile-project-2.jpg',
+			},
+		],
+		ProjectInfo: {
+			ClientHeading: 'About Client',
+			CompanyInfo: [
+				{
+					id: uuidv4(),
+					title: 'Name',
+					details: 'Cetas AI',
+				},
+				{
+					id: uuidv4(),
+					title: 'Services',
+					details: 'Bringing innovations to cybersecurity',
+				},
+				{
+					id: uuidv4(),
+					title: 'Website',
+					details: 'https://www.cetas.ai/',
+				},
+				{
+					id: uuidv4(),
+					title: 'Phone',
+					details: '',
+				},
+			],
+			ObjectivesHeading: 'Objective',
+			ObjectivesDetails:
+				"Cetas strives to be the “central nervous system” of cybersecurity operations, able to learn and cut through the noise to provide confidence-inducing, actionable alerts that meet the needs of today's modern infrastructure, and the constantly evolving AI-driven technology stack of the future. Our Vision and Mission anchor our team's efforts and focus on bringing ground-breaking innovations to cybersecurity.",
+			Technologies: [
+				{
+					title: 'Tools & Technologies',
+					techs: [
+						'NextJs',
+						'Python',
+						'SpringBoot'
+					],
+				},
+			],
+			ProjectDetailsHeading: 'Challenge',
+			ProjectDetails: [
+				{
+					id: uuidv4(),
+					details:
+						"WE MODERNIZE SECURITY OPERATIONS AND EMPOWER SOC TEAMS TO PROTECT BUSINESS-CRITICAL APPLICATIONS AND DATA Headquartered in the San Francisco Bay Area, Cetas was founded by cybersecurity industry veterans to build the world's leading AI-based incident-detection technology.",
+				},
+				{
+					id: uuidv4(),
+					details:
+						"The etymology of the word Cetas is from the ancient Sanskrit language and means consciousness. We apply the philosophy by teaching our AI how to think like a conscious human security analyst. The seismic data explosion due to modern cloud-native immutable infrastructure has made it excruciating to identify critical signals that constitute the fine line between a secure organization and a breached one. Cetas has been at the vanguard of Cybersecurity and has witnessed teams inundated in false positives, and struggling with inefficiencies in archaic, investigative practices. With our deep expertise in machine learning, data science, user experience, and security, we made a promise to build solutions that provide analysts with the best and most accurate information available, eliminate the fog of alerts, clearly visualize threats, and offer a quick, accurate and automated path to remediation. Thus empowering them to create a robust cyber defense.",
+				},
+				{
+					id: uuidv4(),
+					details:
+						'Investments in AI are growing rapidly across businesses and organizations of all types, yet it is very challenging to harvest value from machine learning models and ensure business adoption. With a goal to solve the challenges, our co-founders started Cetas, a Business ValueOps and AI Observability platform to help in the last mile adoption and continuous value realization of AI initiatives.',
+				},
+				{
+					id: uuidv4(),
+					details:
+						"Cetas provides a framework and platform to create, communicate and deliver value continuously from AI models. We are an AI Value Engineering Company and pioneer in monetizing AI and guarantee continuous business value from every AI model. Cetas is on a mission to bridge the gap between AI outcomes and business value realization, increase adoption, extract value and enhance ROI from AI Projects. Cetas empowers business users by being their guide in making strategic decisions. Headquartered in the San Francisco Bay Area and with team members located across the USA and India. We believe AI is a fundamentally trans-formative technology for business. We’re excited about creating the technology that democratizes AI, enabling business people, data scientists and AI strategists to unleash the power of AI and maximize business value",
+				},
+			],
+		},
+	},
 ];
